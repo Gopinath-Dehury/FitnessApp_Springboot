4391195458456e556a0039339ef3878d32ef0dbe
Gopinath Dehury <129596617+Gopinath-Dehury@users.noreply.github.com>
2025-11-05 00:01:35 +0530

    Add log message to indicate application is running

--- src/main/java/com/fitnessApp/FitnessAppApplication.java
+++ src/main/java/com/fitnessApp/FitnessAppApplication.java
@@ -8,6 +8,7 @@ public class FitnessAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FitnessAppApplication.class, args);
+		System.out.println("Its Running perfectly");
 	}
 
 }
