4e7954cb7a04f7375ca0c032f0db2147d517786f
Gopinath Dehury <gopinath90biku@gmail.com>
2025-11-05 01:00:54 +0530

    Refactor pom.xml and FitnessAppApplication.java for improved structure and clarity

--- src/main/java/com/fitnessApp/FitnessAppApplication.java
+++ src/main/java/com/fitnessApp/FitnessAppApplication.java
@@ -1,14 +1,38 @@
+//package com.fitnessApp;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//@SpringBootApplication
+//public class FitnessAppApplication {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(FitnessAppApplication.class, args);
+//		System.out.println("Its Running perfectly");
+//	}
+//
+//}
+
+
 package com.fitnessApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class FitnessAppApplication {
+public class FitnessAppApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(FitnessAppApplication.class, args);
-		System.out.println("Its Running perfectly");
-	}
+    // Used when deploying WAR to external Tomcat
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(FitnessAppApplication.class);
+    }
 
+    // Used when running as a JAR (embedded Tomcat)
+    public static void main(String[] args) {
+        SpringApplication.run(FitnessAppApplication.class, args);
+        System.out.println("Its Running perfectly");
+    }
 }
